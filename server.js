ac762f960ce810a5728e8c12106806ca30a2a459
Charles-Okoeguale <egualecharles@gmail.com>
2024-11-20 21:15:42 +0100

    fix

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ require('dotenv').config();
 const OpenAI = require("openai");
 const path = require('path');
 
-
 const app = express();
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
-
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "/tmp/uploads/" });
 
 app.use(cors({
     origin: ['http://localhost:3000'],
@@ -31,6 +29,7 @@ app.post("/transcribe-audio", upload.single("audio"), async (req, res) => {
             model: "whisper-1",
         });
         await fs.promises.unlink(newPath);
+
         res.json({ text: transcription.text });
     } catch (error) {
         console.error("Error during transcription:", error);
@@ -39,4 +38,3 @@ app.post("/transcribe-audio", upload.single("audio"), async (req, res) => {
 });
 
 module.exports = app;
-
